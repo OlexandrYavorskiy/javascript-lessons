6527d934ce34b3223a7b445471c6fe8294022062
OlexandrYavorskiy <futqegaming@gmail.com>
2024-02-26 14:13:35 +0200

    fixed

--- homework/main2.js
+++ homework/main2.js
@@ -186,7 +186,8 @@ if (number1>number2){
 /*- є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
 за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)*/
 
-let age = 10
+let asd = '' || 'def'
+console.log(asd)
 
 
 
@@ -194,3 +195,31 @@ let age = 10
 
 
 /*- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".*/
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4},
+    ]
+if (coursesAndDurationArray[0].monthDuration > 5 ) {
+    console.log('Super')
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('SUPER')
+}
+if (coursesAndDurationArray[2].monthDuration > 5){
+    console.log('SUPER')
+}
+if (coursesAndDurationArray[3].monthDuration > 5){
+    console.log('super')
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('SUPER')
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('SUPER')
+}
